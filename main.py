cd74969559bdf858570810f5146073a2805ee907
Gabriel Duran <gabrieldurans999@gmail.com>
2026-03-28 10:14:15 -0300

    se añaden archivo main.py, actuliza requiremnts.txt y se agrega en env ecample

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-
 from fastapi import FastAPI
+import requests
+import os
+from dotenv import load_dotenv #liberia para hacer
+
+# 1. Le decimos a Python que lea el archivo .env y cargue los secretos
+load_dotenv()
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"mensaje": "¡Mi entorno virtual esta funcionando"}
+# Creacion de ruta
+@app.get("/api/personaje")
+def obtener_personajes():
+    
+    #Variable que guarda el url de supabase hacia la tabla PERSONAJES
+    url_supabase = "https://izlcprswslrsmunpklny.supabase.co/rest/v1/PERSONAJE"
+    
+    #Contraseña desde el archivo env
+    mi_llave = os.getenv("SUPA_BASE_KEY")
+    
+    # Diccionario con las cabeceras
+    cabeceras = {
+        "apikey": mi_llave,
+    }
+    
+    #Hacemos la llamada GET a Supabase
+    respuesta = requests.get(url_supabase, headers=cabeceras)
+    
+    # Devuelve el json
+    return respuesta.json()
